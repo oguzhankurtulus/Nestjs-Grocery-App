36ea80373b40c1df78953e8657b878a7a62861ee
Oguzhan Kurtulus <oguzhan.kurtulus@yandex.com>
2022-06-30 14:38:42 +0300

    Add middleware and health check.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod
+} from '@nestjs/common';
+import { logger } from './logger.middleware';
+import { HealthModule } from './modules/health/health.module';
 import { ProductModule } from './modules/product/product.module';
 import { WarehouseModule } from './modules/warehouse/warehouse.module';
 
 @Module({
-  imports: [ProductModule, WarehouseModule],
+  imports: [ProductModule, WarehouseModule, HealthModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(logger)
+      .exclude({ path: 'product', method: RequestMethod.GET })
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
---
+++ src/logger.middleware.ts
@@ -0,0 +1,7 @@
+import { NextFunction, Request, Response } from 'express';
+
+export function logger(req: Request, res: Response, next: NextFunction) {
+  console.log('Request', req);
+  console.log('Response', res);
+  next();
+}
